93db0e826eabbd45676d44dc6bb20f938f94974c
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-14 21:24:36 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
 class Meal
 
-end
+attr_accessor :water, :customer, :total, :tip 
+
+@@all [ ]
+
+  def initializes(waiter, customer, total, tip=0)
+    @waiter = waiter 
+    @customer = customer 
+    @total = total 
+    @tip = tip 
+    @@all << self 
+  end
+
+  def self.all 
+    @@all 
+  end 
+end 
